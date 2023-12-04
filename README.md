01f7393b0d43995176d2c0e9205fe0d3ca06e0e2
KidiIT <146373396+KidiIT@users.noreply.github.com>
2023-12-04 02:01:01 -0600

    Added Javascript file on my repo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Shuffle array using sort method with javascript (step1)
+Awesome step to shuffle an Array using the sort method in minutes.
+
+
+
  
---
+++ shuffle.js
@@ -0,0 +1,13 @@
+let a= [1,2,3,4,5,6,7]
+let shuffle= alert(a[Math.floor(Math.random()* a.length )]);
+    const arr=[a.length-1];
+    for( let a=0; a < 10; a++) {
+        arr.sort(a[a.length-1]),a[a.length-3];
+        arr.sort(a[a.length-1]),a[a.length-2];
+        arr.sort(a[a.length-1]),a[a.length-1];
+        arr.sort(a[a.length-1]),a[a.length-0];
+        arr.sort(a[a.length-1]),a[a.length+1];
+       
+
+
+    }
